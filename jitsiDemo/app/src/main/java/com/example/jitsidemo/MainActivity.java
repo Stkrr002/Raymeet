ec01f198eb9a3e6a4de50cd7173fc0c0dc344cf0
stkrr002 <sumitkrr2002@gmail.com>
2022-12-30 23:38:51 +0530

    initial commit

---
+++ jitsiDemo/app/src/main/java/com/example/jitsidemo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.jitsidemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+  EditText edt ;
+  Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edt= findViewById(R.id.edt);
+        btn= findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url=edt.getText().toString();
+                if(url!=null && url!=" ")
+                {
+
+
+                    JitsiMeetConferenceOptions options = null;
+                    try {
+                        options = new JitsiMeetConferenceOptions.Builder()
+                                .setServerURL(new URL("https://meet.jit.si"))
+                                .setRoom(url)
+                                .setFeatureFlag("welcomepage.enabled", false)
+                                .setAudioMuted(false)
+                                .setVideoMuted(false)
+                                .setAudioOnly(false)
+                                .setConfigOverride("requireDisplayName", true)
+                                .build();
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    JitsiMeetActivity.launch(MainActivity.this, options);
+
+                }
+            }
+        });
+    }
+}
