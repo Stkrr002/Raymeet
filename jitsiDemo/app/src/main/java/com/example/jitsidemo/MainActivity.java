75c819dc52f855421930dd2ed02a54a7b0e62f12
stkrr002 <sumitkrr2002@gmail.com>
2023-01-08 01:03:01 +0530

    broadcast issue resolve on hangup

--- jitsiDemo/app/src/main/java/com/example/jitsidemo/MainActivity.java
+++ jitsiDemo/app/src/main/java/com/example/jitsidemo/MainActivity.java
@@ -3,7 +3,12 @@ import static java.text.DateFormat.*;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +23,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.jitsi.meet.sdk.BroadcastEvent;
+import org.jitsi.meet.sdk.BroadcastIntentHelper;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
 import org.jitsi.meet.sdk.JitsiMeetUserInfo;
@@ -29,6 +36,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
   EditText edt ;
@@ -46,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
         String uid= user.getUid();
         Uri imageUrl= user.getPhotoUrl();
           userEmail=user.getEmail();
+
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,21 +76,23 @@ public class MainActivity extends AppCompatActivity {
                                 .setServerURL(new URL("https://meet.jit.si"))
                                 .setRoom(url)
                                 .setFeatureFlag("welcomepage.enabled", false)
-                                .setAudioMuted(false)
-                                .setVideoMuted(false)
-                                .setAudioOnly(false)
-                                .setConfigOverride("requireDisplayName", true)
                                 .setUserInfo(userInfo)
                                 .build();
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
                     }
                     JitsiMeetActivity.launch(MainActivity.this, options);
-
+                    registerForBroadcastMessages();
                 }
             }
         });
     }
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            onBroadcastReceived(intent);
+        }
+    };
     public void uploadData(){
         String title = edt.getText().toString();
         //We are changing the child from title to currentDate,
@@ -104,4 +118,49 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    private void registerForBroadcastMessages() {
+        IntentFilter intentFilter = new IntentFilter();
+
+        /* This registers for every possible event sent from JitsiMeetSDK
+           If only some of the events are needed, the for loop can be replaced
+           with individual statements:
+           ex:  intentFilter.addAction(BroadcastEvent.Type.AUDIO_MUTED_CHANGED.getAction());
+                intentFilter.addAction(BroadcastEvent.Type.CONFERENCE_TERMINATED.getAction());
+                ... other events
+         */
+        for (BroadcastEvent.Type type : BroadcastEvent.Type.values()) {
+            intentFilter.addAction(type.getAction());
+        }
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    // Example for handling different JitsiMeetSDK events
+    private void onBroadcastReceived(Intent intent) {
+        if (intent != null) {
+            BroadcastEvent event = new BroadcastEvent(intent);
+
+            switch (event.getType()) {
+                case CONFERENCE_JOINED:
+                    Timber.i("Conference Joined with url%s", event.getData().get("url"));
+                    break;
+                case PARTICIPANT_JOINED:
+                    Timber.i("Participant joined%s", event.getData().get("name"));
+                    break;
+                case READY_TO_CLOSE:
+                    Toast.makeText(this, "kaat rha h", Toast.LENGTH_SHORT).show();
+                    break;
+                case AUDIO_MUTED_CHANGED:
+                    Toast.makeText(this, "mic changed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Example for sending actions to JitsiMeetSDK
+//    private void hangUp() {
+//        Intent hangupBroadcastIntent = BroadcastIntentHelper.buildHangUpIntent();
+//        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(hangupBroadcastIntent);
+//        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
+//    }
 }
